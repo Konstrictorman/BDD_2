3d6926ee70c51c46f54d3af45d62a945651c76f3
idsanchezf <156478003+idsanchezf@users.noreply.github.com>
2025-02-21 23:14:04 +0000

    Agrego proyecto BDD

---
+++ src/test/java/steps/SearchSteps.java
@@ -0,0 +1,43 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchSteps {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Given("I am on the Google search page")
+    public void i_am_on_the_google_search_page() {
+        driver.get("https://www.google.com");
+    }
+
+    @When("I search for {string}")
+    public void i_search_for(String term) {
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(term);
+        searchBox.submit();
+    }
+
+    @Then("I should see {string} in the results")
+    public void i_should_see_in_the_results(String term) {
+        assert driver.getPageSource().contains(term);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
